198dfddf57825174a44528b92c61836c6ae244c4
tonton-sol <19677766+tonton-sol@users.noreply.github.com>
2024-09-07 22:20:04 +0900

    fix in rust template

--- examples/example01/src/lib.rs
+++ examples/example01/src/lib.rs
@@ -5,7 +5,7 @@ declare_mint!("FQf33CHwMZY4TYo6RP5CuTXUCVs8YFJH1MreMYtHiPhi");
 
 #[transfer_hook]
 pub fn hello_world(ctx: TransferContext<MyExtraMetas>) -> ProgramResult {
-    msg!("Hello from transfer-hook program: {:?}", ctx.program_id);
+    msg!("Ahoy from transfer-hook program: {:?}", ctx.program_id);
     Ok(())
 }
 
