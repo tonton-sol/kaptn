4ccac1f9f6bc65a103d520d6b93d7053e5383a1d
tonton-sol <19677766+tonton-sol@users.noreply.github.com>
2024-09-07 22:58:35 +0900

    rename example

--- examples/example01/src/lib.rs
+++ examples/example01/src/lib.rs
@@ -4,7 +4,7 @@ declare_id!("5H4LbTCzkudomL3ocLttgLFtHWvpbiadS1DhPGvo2XYh");
 declare_mint!("FQf33CHwMZY4TYo6RP5CuTXUCVs8YFJH1MreMYtHiPhi");
 
 #[transfer_hook]
-pub fn hello_world(ctx: TransferContext<MyExtraMetas>) -> ProgramResult {
+pub fn ahoy_world(ctx: TransferContext<MyExtraMetas>) -> ProgramResult {
     msg!("Ahoy from transfer-hook program: {:?}", ctx.program_id);
     Ok(())
 }
