973d6f48cbad4c1b4c09a8a3c04d3c3e8b05ca40
tonton-sol <19677766+tonton-sol@users.noreply.github.com>
2024-09-10 16:57:17 +0900

    add declare mint test

--- lang/tests/macros.rs
+++ lang/tests/macros.rs
@@ -5,6 +5,10 @@ mod id {
     kaptn_lang::declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 }
 
+mod mint {
+    kaptn_lang::declare_mint!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+}
+
 #[test]
 fn test_declare_id() {
     let good = Pubkey::from_str("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS").unwrap();
@@ -13,4 +17,14 @@ fn test_declare_id() {
     assert_eq!(good, id::id());
     assert!(id::check_id(&good));
     assert!(!id::check_id(&bad));
+}
+
+#[test]
+fn test_declare_mint() {
+    let good = Pubkey::from_str("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS").unwrap();
+    let bad = Pubkey::from_str("A7yUYJNEVYRLE4QWsnc9rE9JRsm7DfqEmLscQVwkffAk").unwrap();
+    assert_eq!(good, mint::MINT);
+    assert_eq!(good, mint::mint());
+    assert!(mint::check_mint(&good));
+    assert!(!mint::check_mint(&bad));
 }
