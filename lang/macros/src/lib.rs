9f6a0ef8e91285e9eaa7b9d8aa8ab12afdb5ed0f
tonton-sol <19677766+tonton-sol@users.noreply.github.com>
2024-09-10 15:04:13 +0900

    fix id and mint macros

--- lang/macros/src/lib.rs
+++ lang/macros/src/lib.rs
@@ -1,75 +1,104 @@
+extern crate proc_macro;
+
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, LitStr};
+use proc_macro2::Span;
+use quote::{quote, ToTokens};
+use syn::{
+    parse::{Parse, ParseStream, Result},
+    Expr, LitByte, LitStr,
+};
+
+fn parse_id(input: ParseStream) -> Result<proc_macro2::TokenStream> {
+    let pubkey_type = quote! { kaptn_lang::solana_program::pubkey::Pubkey };
+
+    let id = if input.peek(syn::LitStr) {
+        let id_literal: LitStr = input.parse()?;
+        parse_pubkey(&id_literal, &pubkey_type)?
+    } else {
+        let expr: Expr = input.parse()?;
+        quote! { #expr }
+    };
+
+    if !input.is_empty() {
+        let stream: proc_macro2::TokenStream = input.parse()?;
+        return Err(syn::Error::new_spanned(stream, "unexpected token"));
+    }
+    Ok(id)
+}
+
+fn generate_id_tokens(id: &proc_macro2::TokenStream, name: &str) -> proc_macro2::TokenStream {
+    let pubkey_type = quote! { kaptn_lang::solana_program::pubkey::Pubkey };
+    let name_upper = syn::Ident::new(&name.to_uppercase(), Span::call_site());
+    let name_lower = syn::Ident::new(name, Span::call_site());
+    let check_fn = syn::Ident::new(&format!("check_{}", name), Span::call_site());
+    let const_fn = syn::Ident::new(&format!("{}_const", name), Span::call_site());
+    let name_upper_const =
+        syn::Ident::new(&format!("{}_CONST", name.to_uppercase()), Span::call_site());
+    let array_const = syn::Ident::new(&format!("{}_ARRAY", name.to_uppercase()), Span::call_site());
+
+    quote! {
+        const #array_const: [u8; 32] = #id.to_bytes();
+
+        /// The static ID
+        pub static #name_upper: #pubkey_type = #pubkey_type::new_from_array(#array_const);
+
+        /// Const version of the ID
+        pub const #name_upper_const: #pubkey_type = #pubkey_type::new_from_array(#array_const);
+
+        /// Confirms that a given pubkey is equivalent to the ID
+        pub fn #check_fn(id: &#pubkey_type) -> bool {
+            id.to_bytes() == #array_const
+        }
+
+        /// Returns the ID
+        pub fn #name_lower() -> #pubkey_type {
+            #pubkey_type::new_from_array(#array_const)
+        }
+
+        /// Const version of the ID function
+        pub const fn #const_fn() -> #pubkey_type {
+            #name_upper_const
+        }
+    }
+}
+
+fn parse_pubkey(
+    id_literal: &LitStr,
+    pubkey_type: &proc_macro2::TokenStream,
+) -> Result<proc_macro2::TokenStream> {
+    let id_vec = bs58::decode(id_literal.value())
+        .into_vec()
+        .map_err(|_| syn::Error::new_spanned(id_literal, "failed to decode base58 string"))?;
+    let id_array = <[u8; 32]>::try_from(<&[u8]>::clone(&&id_vec[..])).map_err(|_| {
+        syn::Error::new_spanned(
+            id_literal,
+            format!("pubkey array is not 32 bytes long: len={}", id_vec.len()),
+        )
+    })?;
+    let bytes = id_array.iter().map(|b| LitByte::new(*b, Span::call_site()));
+    Ok(quote! {
+        #pubkey_type::new_from_array(
+            [#(#bytes,)*]
+        )
+    })
+}
+
+struct Id(proc_macro2::TokenStream);
+
+impl Parse for Id {
+    fn parse(input: ParseStream) -> Result<Self> {
+        parse_id(input).map(Self)
+    }
+}
 
 #[proc_macro]
 pub fn declare_id(input: TokenStream) -> TokenStream {
-    let id_str = parse_macro_input!(input as LitStr);
-    let id_bytes: Vec<u8> = id_str.value().as_bytes().iter().take(32).copied().collect();
-
-    if id_bytes.len() != 32 {
-        panic!("The provided ID must be exactly 32 bytes long.");
-    }
-
-    let id_array = quote! {
-        [#(#id_bytes),*]
-    };
-
-    let expanded = quote! {
-        pub static ID: kaptn_lang::prelude::Pubkey = kaptn_lang::prelude::Pubkey::new_from_array(#id_array);
-        pub const ID_CONST: kaptn_lang::prelude::Pubkey = kaptn_lang::prelude::Pubkey::new_from_array(#id_array);
-
-        pub fn check_id(id: &kaptn_lang::prelude::Pubkey) -> bool {
-            id == &ID
-        }
-
-        pub fn id() -> kaptn_lang::prelude::Pubkey {
-            ID
-        }
-
-        pub const fn id_const() -> kaptn_lang::prelude::Pubkey {
-            ID_CONST
-        }
-    };
-
-    TokenStream::from(expanded)
+    let id = syn::parse_macro_input!(input as Id);
+    TokenStream::from(generate_id_tokens(&id.0, "id"))
 }
 
 #[proc_macro]
 pub fn declare_mint(input: TokenStream) -> TokenStream {
-    let mint_str = parse_macro_input!(input as LitStr);
-    let mint_bytes: Vec<u8> = mint_str
-        .value()
-        .as_bytes()
-        .iter()
-        .take(32)
-        .copied()
-        .collect();
-
-    if mint_bytes.len() != 32 {
-        panic!("The provided mint must be exactly 32 bytes long.");
-    }
-
-    let mint_array = quote! {
-        [#(#mint_bytes),*]
-    };
-
-    let expanded = quote! {
-        pub static MINT: kaptn_lang::prelude::Pubkey = kaptn_lang::prelude::Pubkey::new_from_array(#mint_array);
-        pub const MINT_CONST: kaptn_lang::prelude::Pubkey = kaptn_lang::prelude::Pubkey::new_from_array(#mint_array);
-
-        pub fn check_mint(mint: &kaptn_lang::prelude::Pubkey) -> bool {
-            mint == &MINT
-        }
-
-        pub fn mint() -> kaptn_lang::prelude::Pubkey {
-            MINT
-        }
-
-        pub const fn mint_const() -> kaptn_lang::prelude::Pubkey {
-            MINT_CONST
-        }
-    };
-
-    TokenStream::from(expanded)
+    let id = syn::parse_macro_input!(input as Id);
+    TokenStream::from(generate_id_tokens(&id.0, "mint"))
 }
